3a2394109f7e15930ca2c0da3d7f196bd5b04318
yaswethaap-arch <ya.swethaap@gmail.com>
2025-12-27 12:12:07 +0530

    Update server.py
    Modified for dominant_emotion

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ def emotion_detector():
     response = emotion_detector(text_to_analyze)
   
     # Return a formatted string with the emotion response
-    return "For the given statement, the system response is 'anger':{}, 'disgust': {}, 'fear':{}, 'joy' :{}, 'sadness' :{}.The dominant emotion is {}".format(response['anger'], response['disgust'], response['fear'], response['joy'], response['sadness'], response['dominantEmotion'])
+    return "For the given statement, the system response is 'anger':{}, 'disgust': {}, 'fear':{}, 'joy' :{}, 'sadness' :{}.The dominant emotion is {}.".format(response['anger'], response['disgust'], response['fear'], response['joy'], response['sadness'], response['dominant_emotion'])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
