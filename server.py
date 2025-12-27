6ed35ffe40b4eca0dc9b543bf74312faf5e227c9
yaswethaap-arch <ya.swethaap@gmail.com>
2025-12-27 13:08:29 +0530

    Update server.py
    Added docstrings and f-string

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Import flask module"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,18 +6,25 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    """Function navigating to index page"""
     return render_template('index.html')
 
 @app.route("/emotionDetector")
-def emotion_detector():
+def emot_detector():
+    """Funtion to detect emotion of an input text"""
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
-  
     # Return a formatted string with the emotion response
-    return "For the given statement, the system response is 'anger':{}, 'disgust': {}, 'fear':{}, 'joy' :{}, 'sadness' :{}.The dominant emotion is {}.".format(response['anger'], response['disgust'], response['fear'], response['joy'], response['sadness'], response['dominant_emotion'])
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please Try again."
+    return f"For the given statement, the system response is \
+                'anger':{response['anger']}, 'disgust': {response['disgust']},\
+                'fear':{response['fear']}, 'joy' :{response['joy']},\
+                'sadness' :{response['sadness']}.\
+                The dominant emotion is {response['dominant_emotion']}."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
